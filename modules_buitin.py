eec24f86b1a4f24b09bb4ff6b2b77e767c797dc5
Siphumelele Ndlovu <siphumelelendlovu5@gmail.com>
2025-07-04 23:14:18 +0200

    Add files via upload

---
+++ modules_buitin.py
@@ -0,0 +1,12 @@
+import math
+import random
+import datetime
+import os
+import sys
+
+
+#print(math.pi)
+#print(random.randint(0,100))
+#print(random.randint(0, 100)) 
+#print(os.getcwd())
+print(sys.version)
